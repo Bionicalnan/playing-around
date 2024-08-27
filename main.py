07f5fc9f6e4af854f87b15ab3ffc4d758c24c063
Bionicalnan <94143778+Bionicalnan@users.noreply.github.com>
2024-08-27 13:21:18 -0700

    Update main..py
    having fun

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-"""
-(headline) BIKE MECHANIC BUSINESS INVENTORY: bold, uppercase, red & underlined
+print('''
+----------------------
+|                    |
+|                    | 
+|   Hello, world!    |
+|                    |   
+|                    |    
+----------------------''')
 
-(h1) TOOLS: tab 2x, underline, bold, uppercase & purple
 
-WRENCHES: underlined & uppercase & tab 1x
-
-Dog Bone(bold tab 1x): 5/8-1, 1/16, 3/4-7/8, 7/16-1/2 & 9/16/32
-
-Cresent(bold): 15"-375mm
-
-Cresent(bold): 6"150mm"""''
-
-print("BIKE BUSINESS INVENTORY:")
-print('\n' '\t' 'Wrench:' '\n' '\n' '\t' '\t' 'multi-cresent' '\t' 'one' '\n' '\t' '\t' 'large-cresent' '\t' 'one' '\n' '\t' '\t' 'small-cresent' '\t' 'one')
+print(''' -------------
+    0         |
+    0         | 
+    #         |
+ ---+---      |
+    |         | 
+    |         | 
+   ---        |  
+   |  |       |''')
